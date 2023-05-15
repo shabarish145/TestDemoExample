7f85bd2972ad984a57fe6fef8a685517207d6d6b
shabarish145 <shabarish145@gmail.com>
2023-05-15 11:09:09 +0530

    Added readme

---
+++ src/JenkinsIntegration/JenkinsIntegrationDemo.java
@@ -0,0 +1,38 @@
+package JenkinsIntegration;
+
+import org.testng.annotations.Test;
+
+public class JenkinsIntegrationDemo {
+
+	@Test
+	public void TestOne() {
+		System.out.println("Test case1 passed");
+	}
+
+	@Test
+	public void TestTwo() {
+		System.out.println("Test case2 passed");
+
+	}
+
+	@Test
+	public void TestThree() {
+		System.out.println("Test case3 passed");
+		System.out.println("Welcome");
+		System.out.println("Jenkins are used to CI/CD");
+
+	}
+
+	@Test
+	public void TestFour() {
+		System.out.println("Test case4 passed");
+
+	}
+
+	@Test
+	public void TestFive() {
+		System.out.println("Test case5 passed");
+
+	}
+
+}
